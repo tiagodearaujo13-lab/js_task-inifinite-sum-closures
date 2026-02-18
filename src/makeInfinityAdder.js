051bd0cc0c0592799681957de188492297ed5ea5
Tiago <o-seu-email-do-github@exemplo.com>
2026-02-18 16:28:09 +0000

    task js_task-inifinite-sum-closures done!

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -5,6 +5,23 @@
  */
 function makeInfinityAdder() {
   // write code here
+  let sum = 0;
+
+  const adder = (num) => {
+    if (num === undefined) {
+      const results = sum;
+
+      sum = 0;
+
+      return results;
+    } else {
+      sum += num;
+
+      return adder;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
